d7f15f1832aa874472aa564f9b93da04f1ad7ba7
parfait92-git <parfaitaristantnzo@gmail.com>
2025-05-24 22:59:11 -0400

    my first commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component, ViewEncapsulation } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss',
+  encapsulation: ViewEncapsulation.None
+})
+export class AppComponent {
+  title = 'porfolio';
+}
